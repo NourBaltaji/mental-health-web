bd722393811f2477b5205f360c3e2102a51394d6
blo0dyred <mahmoud.othman.ajam@gmail.com>
2021-07-27 21:11:27 +0300

    Navbar triel using bootsrap but it's not working properly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import React from "react";
+import NavigationBar from "./Components/NavigationBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
-      </header>
+    <div>
+      <NavigationBar/>
     </div>
   );
 }
