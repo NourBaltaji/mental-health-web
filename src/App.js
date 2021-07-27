081140219e1805f9affa32d292c363feb1807369
Amro-dandashli <amrdandashli@gmail.com>
2021-07-27 13:45:19 +0300

    creating the blogpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
 import React from "react";
-
+import Blog from "./blog";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
-      </header>
-    </div>
+   <Blog />
   );
 }
 
