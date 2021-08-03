8ca6ad9d51cb206293599e9436347241f2f156ca
Mohamad <mohamad@devvly.com>
2021-08-03 21:24:55 +0300

    code for contact us page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
+import ContactUs from "./ContactUs";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Welcome to the capstone projects, this is your initial react app</p>
+        {/* <p>Welcome to the capstone projects, this is your initial react app</p> */}
+        <ContactUs/>
       </header>
     </div>
   );
